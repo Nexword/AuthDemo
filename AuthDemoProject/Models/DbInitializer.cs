7629ef01bc7317cc848862ce89e8fe28794d3be4
Mikhail Dudin <dudin-m@live.ru>
2017-04-17 23:54:01 +0300

    DbInitializer

---
+++ AuthDemoProject/Models/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AuthDemoProject.Models
+{
+	public class DbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+	{
+		protected override void Seed(ApplicationDbContext context)
+		{
+			var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+			var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+			var adminRole = new IdentityRole { Name = "admin" };
+
+			roleManager.Create(adminRole);
+
+			var admin = new ApplicationUser { Email = "somemail@mail.ru", UserName = "somemail@mail.ru" };
+			var password = "ad46D_ewr3";
+			var result = userManager.Create(admin, password);
+
+			if (result.Succeeded)
+			{
+				userManager.AddToRole(admin.Id, adminRole.Name);
+			}
+
+			base.Seed(context);
+		}
+	}
+}
